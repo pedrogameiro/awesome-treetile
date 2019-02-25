aef02e502eb518fdb2c41eeb1a6c8d7f97aa10a5
Alphonse Mariya <alphonse.mariya@hotmail.com>
2019-02-25 22:35:00 +0100

    Add method to search tree using a predicate

--- bintree.lua
+++ bintree.lua
@@ -12,7 +12,6 @@
 
 local table        = table
 local tostring     = tostring
-local type         = type
 
 local bintree = {}
 bintree.__index = bintree
@@ -25,36 +24,52 @@ function bintree.new(data, left, right)
     }, bintree)
 end
 
+-- New left node (forwards to constructor)
 function bintree:set_new_left(...)
     self.left = bintree.new(...)
     return self.left
 end
 
+-- New right node (forwards to constructor)
 function bintree:set_new_right(...)
     self.right = bintree.new(...)
     return self.right
 end
 
+-- Set left node
 function bintree:set_left(node)
     self.left = node
     return self.left
 end
 
+-- Set right node
 function bintree:set_right(node)
     self.right = node
     return self.right
 end
 
-function bintree:find(data)
-    if data == self.data then return self end
-
-    return self.left and self.left:find(data)
-        or self.right and self.right:find(data)
+local function get_predicate(data)
+    return function(node)
+        return node.data == data
+    end
 end
 
--- remove leaf and replace parent by sibling
-function bintree:remove(data)
-    if self.left and self.left.data == data then
+-- Get node if predicate returns true
+function bintree:find_if(predicate)
+    if predicate(self) then return self end
+    return self.left and self.left:find_if(predicate)
+        or self.right and self.right:find_if(predicate)
+end
+
+-- Get node for matching data
+function bintree:find(data)
+    return self:find_if(get_predicate(data))
+end
+
+-- Remove leaf node if predicate returns true
+-- (Removes a node, parent is replaced by sibling.)
+function bintree:remove_if(predicate)
+    if self.left and predicate(self.left) then
         local new_self = {
             data = self.right.data,
             left = self.right.left,
@@ -63,10 +78,10 @@ function bintree:remove(data)
         self.data = new_self.data
         self.left = new_self.left
         self.right = new_self.right
-        return true
+        return self
     end
 
-    if self.right and self.right.data == data then
+    if self.right and predicate(self.right) then
         local new_self = {
             data = self.left.data,
             left = self.left.left,
@@ -75,84 +90,79 @@ function bintree:remove(data)
         self.data = new_self.data
         self.left = new_self.left
         self.right = new_self.right
-        return true
+        return self
     end
 
     if self.left then
-        local output = self.left:remove(data)
+        local output = self.left:remove_if(predicate)
         if output then return output end
     end
 
     if self.right then
-        local output = self.right:remove(data)
+        local output = self.right:remove_if(predicate)
         if output then return output end
     end
 end
 
-function bintree:remove_if(filter)
-    if self.left and filter(self.left) then
-        self:remove(self.left.data)
-        return true
-    end
+-- Remove leaf node for matching data
+function bintree:remove(data)
+    return self:remove_if(get_predicate(data))
+end
 
-    if self.right and filter(self.right) then
-        self:remove(self.right.data)
-        return true
-    end
+-- Get sibling of node where predicate returns true
+function bintree:get_sibling_if(predicate)
+    if predicate(self) then return nil end
 
     if self.left then
-        local output = self.left:remove_if(filter)
+        if predicate(self.left) then return self.right end
+        local output = self.left:get_sibling_if(predicate)
         if output then return output end
     end
 
     if self.right then
-        local output = self.right:remove_if(filter)
+        if predicate(self.right) then return self.left end
+        local output = self.right:get_sibling_if(predicate)
         if output then return output end
     end
 end
 
+-- Get sibling of node for matching data
 function bintree:get_sibling(data)
-    if data == self.data then return nil end
+    return self:get_sibling_if(get_predicate(data))
+end
+
+-- Get parent of node where predicate returns true
+function bintree:get_parent_if(predicate)
+    if predicate(self) then return nil end
 
     if self.left then
-        if self.left.data == data then
-            return self.right
-        end
-
-        local output = self.left:get_sibling(data)
+        if predicate(self.left) then return self end
+        local output = self.left:get_parent_if(predicate)
         if output then return output end
     end
 
     if self.right then
-        if self.right.data == data then
-            return self.left
-        end
-
-        local output = self.right:get_sibling(data)
+        if predicate(self.right) then return self end
+        local output = self.right:get_parent_if(predicate)
         if output then return output end
     end
 end
 
+-- Get parent of node for matching data
 function bintree:get_parent(data)
-    if self.left then
-        if self.left.data == data then
-            return self
-        end
-
-        local output = self.left:get_parent(data)
-        if output then return output end
-    end
-
-    if self.right then
-        if self.right.data == data then
-            return self
-        end
-
-        local output = self.right:get_parent(data)
-        if output then return output end
-    end
+    return self:get_parent_if(get_predicate(data))
 end
 
+-- Swap two nodes where predicate returns true
+function bintree:swap_leaves_if(predicate1, predicate2)
+    local leaf1 = self:find_if(predicate1)
+    local leaf2 = self:find_if(predicate2)
+
+    if not (leaf1 and leaf2) then return end
+    leaf1.data, leaf2.data = leaf2.data, leaf1.data
+end
+
+-- Swap two nodes for matching data
 function bintree:swap_leaves(data1, data2)
     local leaf1 = self:find(data1)
     local leaf2 = self:find(data2)
@@ -161,6 +171,7 @@ function bintree:swap_leaves(data1, data2)
     leaf1.data, leaf2.data = leaf2.data, leaf1.data
 end
 
+-- Print tree
 function bintree.show(node, level)
     if not level then level = 0 end
     if not node then return end
